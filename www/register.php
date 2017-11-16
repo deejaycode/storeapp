d6e248f7df34c461b07dad91da7df66764ad56d0
deejaycode <dennis.ozj@gmail.com>
2017-11-16 15:55:30 +0100

    PDO insert query written

--- www/register.php
+++ www/register.php
@@ -3,6 +3,64 @@
 	$page_title = "Register";
 	include 'include/header.php';
 
+	include 'include/db.php';
+
+	$errors = [];
+
+		if(array_key_exists('register',$_POST)){
+
+				
+
+				if(empty($_POST['fname'])){
+					$errors['fname'] = "Please enter firstname";
+				}
+
+				if(empty($_POST['lname'])){
+
+					$errors['lname'] = "please enter lastname";
+				}
+				if(empty($_POST['email'])){
+
+					$errors['email'] = "Please enter email";
+				}
+				if(empty($_POST['password'])){
+
+					$errors['password'] = "Please enter password";
+				}
+				if(empty($_POST['pword'])){
+
+					$errors['pword'] = "Please confirm your password";
+				}
+
+				if(empty($errors)){
+					#do database stuff
+					$clean = array_map('trim', $_POST);
+
+					$hash = password_hash($clean['password'], PASSWORD_BCRYPT);
+
+					$stmt = $conn->prepare("INSERT INTO admin(firstName, lastName, email, hash) VALUES(:f, :l, :e, :h)");
+
+					#data binding is done here
+
+					$data = [
+							":f" => $clean['fname'],
+							":l" => $clean['lname'],
+							":e" => $clean['email'],
+							":h" => $hash
+					];
+
+					$stmt->execute($data);
+
+
+				}
+
+
+
+		}
+
+
+	
+
 
 ?>
 
@@ -11,24 +69,29 @@
 		<hr>
 		<form id="register"  action ="register.php" method ="POST">
 			<div>
+				<?php if(isset($errors['fname'])) { echo '<span class=err>'.$errors['fname'].'</span>'; } ?>
 				<label>first name:</label>
 				<input type="text" name="fname" placeholder="first name">
 			</div>
 			<div>
+				<?php if(isset($errors['lname'])) { echo '<span class=err>'.$errors['lname'].'</span>'; } ?>
 				<label>last name:</label>	
 				<input type="text" name="lname" placeholder="last name">
 			</div>
 
 			<div>
+				<?php if(isset($errors['email'])) { echo '<span class=err>'.$errors['email'].'</span>'; } ?>
 				<label>email:</label>
 				<input type="text" name="email" placeholder="email">
 			</div>
 			<div>
+				<?php if(isset($errors['password'])) { echo '<span class=err>'.$errors['password'].'</span>'; } ?>
 				<label>password:</label>
 				<input type="password" name="password" placeholder="password">
 			</div>
  
 			<div>
+				<?php if(isset($errors['pword'])) { echo '<span class=err>'.$errors['pword'].'</span>'; } ?>
 				<label>confirm password:</label>	
 				<input type="password" name="pword" placeholder="password">
 			</div>
