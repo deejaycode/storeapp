0d24d3bb56c7c4342852a716eb9bfc75fbfc68bd
deejaycode <dennis.ozj@gmail.com>
2017-11-16 13:57:24 +0100

    setting up environment for bookstore

--- www/test.php
+++ www/test.php
@@ -1,23 +1,76 @@
+<!--script to handle upload -->
+
+
+
 <?php
 
+	include ('include/function.php');
 
-	define('DBNAME','store');
-	define('DBUSER','root');
-	define('DBPASS','chygor');
+	define('MAX_FILE_SIZE', '2097152');
+	$ext = ['image/jpg','image/jpeg','image/png'];
 
+	if(array_key_exists('save', $_POST)){
 
-	try {
+		//print_r($_FILES);
 
-	$conn = new PDO('mysql:host-localhost;dbname'.DBNAME, DBUSER, DBPASS);
+		$error = [];
 
-	$conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRORMODE_SILENT);
+		if(empty($_FILES['pics']['name'])){
+			$error[] ="Please select a file";
+		}
 
-	}
+		if($_FILES['pics']['size'] > MAX_FILE_SIZE) {
+			$error[] = "File too large. Maximum: ".MAX_FILE_SIZE;
+			$_FILES['pics']['tmp_name'] = null;
+		}
 
-	catch (PDOException $err) {
-		echo $err->getMessage();
+		if(!in_array($_FILES['pics']['type'], $ext)){
+			$error[] = "file format is not supported";
+		}
 
+		/*
+		$strip_name = str_replace(' ','_',$_FILES['pics']['name']);
+
+		$filename = setRandom().$strip_name;
+		$destination = './uploads/'.$filename;*/
+
+		/*if(!move_uploaded_file($_FILES['pics']['tmp_name'], $destination)){
+				$error[] = "File not uploaded";
+		} */
+
+		if(empty($error)){
+			//move_uploaded_file($_FILES['pics']['tmp_name'], $destination);
+			$msg = uploadFile($_FILES, 'pics', 'uploads/');
+
+			if($msg[0]){
+				echo $msg[0];
+			}
+			
+		}else {
+			foreach ($error as $err) {
+				echo $err.'<br/>';
+			}
+		}
 	}
 
 
 ?>
+
+
+
+
+
+
+<form id="register" method="POST" enctype="multipart/form-data">
+
+	<p>Please Upload a picture </p>
+	<input type="file" name="pics">
+
+	<input type="submit" name="save">
+
+
+
+
+
+
+</form>
