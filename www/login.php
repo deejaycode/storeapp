916c994e7d51910a8c7871295c838e4d4c4113ea
deejaycode <dennis.ozj@gmail.com>
2017-11-18 00:19:15 +0100

    validation to check password input

--- www/login.php
+++ www/login.php
@@ -1,33 +1,67 @@
 <?php
 	
+	session_start();
 	$page_title = "Login";
 	include 'include/header.php';
 	include 'include/db.php';
 	include 'include/function.php';
 
+			$error = [];
 
 		if(array_key_exists('register', $_POST)){
 
-				$error = [];
+				
 
 				if(empty($_POST['email'])){
-					$error [] = "Please enter email";
+					$error['email'] = "Please enter email";
 				}
 				if(empty($_POST['password'])){
-					$error[] = "Please enter password";
+					$error['password'] = "Please enter password";
 				}
 				if(empty($error)){
 
 					#do login stuff
+					$removeSpace = array_map('trim',$_POST);
+					$stmt = $conn->prepare("SELECT * FROM admin WHERE :e= email AND :h= password");
+					$data = [
+						":e"=> $_POST['email'],
+						":h"=> $_POST['password'],
+
+					];
+
+					$stmt->execute($data);
+
+					$count = $stmt->rowCount();
+
+					if($count = 1){
+
+						$result = array_values($count);
+
+						$_SESSION['admin_id']= $result['admin_id'];
+						$_SESSION['email'] = $result['email'];
+
+						$message = "congratulations";
+
+						header("location:home.php?message=$message");
+						}
+					else{
+
+						$message = "invalid username and password";
+
+						header("location:login.php?message=$message");
+						}
+
+					}
+
 				}
 				else{
 
-					foreach ($error as $err) {
+					foreach ($error as $error){
 						# code...
-						echo $err.'</br>';
+						echo $error.'</br>';
 					}
 				}
-		}
+		
 
 ?>
 
@@ -38,10 +72,12 @@
 		<hr>
 		<form id="register"  action ="login.php" method ="POST">
 			<div>
+				<?php if(isset($_POST['email'])){ echo '<span class=err>'.$error['email'].'</span>'; } ?>
 				<label>email:</label>
 				<input type="text" name="email" placeholder="email">
 			</div>
 			<div>
+				<?php if(isset($_POST['password'])){ echo '<span class=err>'.$error['password'].'</span>'; } ?>
 				<label>password:</label>
 				<input type="password" name="password" placeholder="password">
 			</div>
